11e2e2958067c5ec57c6871c4c43dddf94e5305b
Mortle <alexander.vinogradov.an@mail.ru>
2020-03-09 03:04:10 +0300

    Added empty tiles cache, improved Tile

--- src/com/hnefashuffle/engine/board/Tile.java
+++ src/com/hnefashuffle/engine/board/Tile.java
@@ -1,6 +1,13 @@
 package com.hnefashuffle.engine.board;
 
 import com.hnefashuffle.engine.pieces.Piece;
+import javafx.util.Pair;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.lang.Math.pow;
 
 /**
  * Created by Mortle on 03/08/20.
@@ -8,21 +15,48 @@ import com.hnefashuffle.engine.pieces.Piece;
 
 public abstract class Tile {
 
+    String type;
     int x;
     int y;
 
-    Tile(int x, int y) {
+    private static final Map<Pair<Integer, Integer>, EmptyTile> EMPTY_TILES_CACHE = createAllPossibleEmptyTiles(11);
+
+    private static Map<Pair<Integer, Integer>, EmptyTile> createAllPossibleEmptyTiles(int boardSize) {
+
+        final Map<Pair<Integer, Integer>, EmptyTile> emptyTileMap = new HashMap<>();
+
+        for(int x = 0; x < pow(boardSize, 2); x++) {
+            for(int y = 0; y < pow(boardSize, 2); y++) {
+
+                if (x == y && x == boardSize / 2) {
+                    emptyTileMap.put(new Pair<>(x, y), new EmptyTile(x, y, "throne"));
+                } else if (x == 0 || y == 0 || x == boardSize - 1 || y == boardSize - 1) {
+                    emptyTileMap.put(new Pair<>(x, y), new EmptyTile(x, y, "corner"));
+                } else {
+                    emptyTileMap.put(new Pair<>(x, y), new EmptyTile(x, y, "default"));
+                }
+            }
+        }
+
+        return Collections.unmodifiableMap(emptyTileMap);
+    }
+
+    private Tile(int x, int y, String type) {
         this.x = x;
         this.y = y;
+        this.type = type;
     }
 
     public abstract boolean isOccupied();
 
     public abstract Piece getPiece();
 
+    public String getType() { return type; }
+
     public static final class EmptyTile extends Tile {
-        EmptyTile(int x, int y) {
-            super(x, y);
+
+        private EmptyTile(final int x, final int y, final String type) {
+            super(x, y, type);
         }
 
         @Override
@@ -38,10 +72,10 @@ public abstract class Tile {
 
     public static final class OccupiedTile extends Tile {
 
-        Piece piece;
+        private Piece piece;
 
-        OccupiedTile(int x, int y, Piece piece) {
-            super(x, y);
+        private OccupiedTile(final int x, final int y, final String type, Piece piece) {
+            super(x, y, type);
             this.piece = piece;
         }
 
