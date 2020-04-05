21537b4961b334ed335672158c5a4baecb2b8020
Mortle <alexander.vinogradov.an@mail.ru>
2020-04-05 08:42:18 +0300

    Showing pieces on tiles

--- src/com/hnefashuffle/gui/Table.java
+++ src/com/hnefashuffle/gui/Table.java
@@ -1,36 +1,44 @@
 package com.hnefashuffle.gui;
 
+import com.hnefashuffle.engine.board.Board;
 import com.hnefashuffle.engine.board.BoardUtils;
 import com.hnefashuffle.engine.board.Coordinates;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Table {
     private JFrame gameFrame;
     private BoardPanel boardPanel;
+    private Board gameBoard;
 
     private static Dimension OUTER_FRAME_DIMENSION = new Dimension(600,600);
     private static Dimension BOARD_PANEL_DIMENSION = new Dimension(440,440);
     private static Dimension TILE_PANEL_DIMENSION = new Dimension(10, 10);
 
+    private static String pieceIconPath = "res/pieces/default/";
+
     private static Color defaultTileColor = Color.decode("#FFB965");
     private static Color cornerTileColor = Color.decode("#925000");
-    private static Color throneTileColor = Color.decode("#593E1A");
+    private static Color throneTileColor = Color.decode("#925000");
     private static Color tileBorderColor = Color.decode("#000000");
 
     public Table() {
+        this.gameBoard = Board.createInitialBoard();
         this.gameFrame = new JFrame("Hnefashuffle");
         this.gameFrame.setLayout(new BorderLayout());
+        this.gameFrame.setSize(OUTER_FRAME_DIMENSION);
 
         JMenuBar tableMenuBar = createTableMenuBar();
         this.gameFrame.setJMenuBar(tableMenuBar);
-
-        this.gameFrame.setSize(OUTER_FRAME_DIMENSION);
 
         this.boardPanel = new BoardPanel();
         this.gameFrame.add(this.boardPanel, BorderLayout.CENTER);
@@ -93,11 +101,26 @@ public class Table {
             this.tileCoordinates = tileCoordinates;
             setPreferredSize(TILE_PANEL_DIMENSION);
             assignTileColor();
+            setBorder(BorderFactory.createLineBorder(tileBorderColor));
+            assignTilePieceIcon(gameBoard);
             validate();
         }
 
+        private void assignTilePieceIcon(Board board) {
+            this.removeAll();
+            if (board.getTile(this.tileCoordinates).isOccupied()) {
+                try {
+                    BufferedImage image =
+                            ImageIO.read(new File(pieceIconPath + board.getTile(this.tileCoordinates).getPiece().getPieceUnion().toString().substring(0, 1) +
+                                    board.getTile(this.tileCoordinates).getPiece().toString() + ".png"));
+                    add(new JLabel(new ImageIcon(image)));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
         private void assignTileColor() {
-            setBorder(BorderFactory.createLineBorder(tileBorderColor));
             if (Coordinates.getCornersCoordinates().contains(this.tileCoordinates)) {
                 setBackground(cornerTileColor);
             } else if (this.tileCoordinates.getXCoordinate() == 5 && this.tileCoordinates.getYCoordinate() == 5) {
