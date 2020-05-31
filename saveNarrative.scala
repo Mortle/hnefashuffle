2491582018236140136e929d72db7f03fb8963b1
Mortle <alexander.vinogradov.an@mail.ru>
2020-05-31 23:24:16 +0300

    Added scala save narrative gen

---
+++ saveNarrative.scala
@@ -0,0 +1,45 @@
+import scala.io.Source
+
+object saveNarrative {
+  def main(args: Array[String]): Unit = {
+    val savePath = args(0)
+
+    printSaveNarrative(savePath)
+  }
+
+  def printSaveNarrative(savePath: String): Unit = {
+    val file = Source.fromFile(savePath)
+
+    val lines = file.getLines()
+
+    lines.next()
+    val movesSeq = lines.next()
+    val tokens = movesSeq.split(", ")
+
+    var moveMaker = "Attackers"
+    for(i <- 0 until tokens.length) {
+      val moveCoords = tokens(i).split(" → ")
+
+      moveMaker = if (moveMaker.equals("Attackers")) "Defenders" else "Attackers"
+
+      printf(
+        "Turn %d: %s player moved piece from %s to %s\n",
+        i + 1,
+        moveMaker,
+        moveCoords(0),
+        moveCoords.last
+      )
+    }
+
+    file.close()
+  }
+}
+
+
+
+
+
+
+
+
+
